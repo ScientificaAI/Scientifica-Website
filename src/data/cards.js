7b05a5aa61403be2515a04562131afb7dd6dc54e
matti909 <matias.saantiago@gmail.com>
2024-06-18 18:26:45 -0300

    Storyteller section added

---
+++ src/data/cards.js
@@ -0,0 +1,6 @@
+export const cards = [
+  { title: "200+", body: "Happy clients", logo: "Icon.svg"},
+  { title: "100M+", body: "Models developed", logo: "Vector.svg"},
+  { title: "300+", body: "Products build", logo: "Vector.svg"},
+  { title: "15+", body: "Countries served", logo: "Icon2.svg"}
+];
