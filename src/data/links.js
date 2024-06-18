6daca3b3d798df321d7ed7979cd2dfef80e46578
matti909 <matias.saantiago@gmail.com>
2024-06-18 11:58:52 -0300

    header added

---
+++ src/data/links.js
@@ -0,0 +1,7 @@
+
+export const navLinks = [
+  { text: 'Home', href: '/' },
+  { text: 'Services', href: '/services' },
+  { text: 'Research', href: '/research' },
+  { text: 'Join Our Networks', href: '/join-our-networks' },
+];
