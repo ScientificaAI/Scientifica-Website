21f87e867ade52adf00338d15198567871a2898b
matti909 <matias.saantiago@gmail.com>
2024-06-24 12:14:30 -0300

    our services section added

---
+++ src/data/card2.js
@@ -0,0 +1,18 @@
+export const card2 = [
+  {
+    title: "Digital Strategy",
+    body: "We approach Digital Strategy with the use of technologies such as artificial intelligence, machine learning , robotics, data analytics, and the Internet of Things (IoT). As these technologies dramatically reshape industries, many companies are pursuing large-scale change efforts to capture the benefits of these trends or simply to keep up with competitors.",
+  },
+  {
+    title: "Digital Product Development",
+    body: "We offer purpose-built business solutions grounded in impeccable, award-winning scientific research. Driven by cutting-edge technology, our AI-powered solutions can streamline business processes, optimize operational performance, and deliver unprecedented efficiency to achieve further growth.",
+  },
+  {
+    title: "Our Models",
+    body: "Our innovative models provide companies with powerful solutions in decision science, credit, finance and pricing. We specialize in reputation modelling to ensure a positive public image; recommendation systems for personalization of marketing efforts; proactive risk management programs that mitigate fraudulent activity and money laundering operations.",
+  },
+  {
+    title: "Research",
+    body: "We offer a comprehensive set of scientific research services that provide sound knowledge and actionable information to businesses regarding their product plans and business problems. Led by a team of subject matter experts, we design and conduct sophisticated studies that meet client needs and deliver answers.",
+  },
+];
