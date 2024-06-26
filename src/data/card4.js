bcd615b02cf8f999069212b76d9cc0e6077027ee
matti909 <matias.saantiago@gmail.com>
2024-06-26 12:21:24 -0300

    services page done

---
+++ src/data/card4.js
@@ -0,0 +1,8 @@
+export const card4 = [
+  { title: "Unmatched Expertise", logo: "bar1.svg" },
+  { title: "Speed of Service", logo: "bar2.svg" },
+  { title: "Proof of Concept", logo: "bar3.svg" },
+  { title: "Inpeccable Reporting", logo: "bar4.svg" },
+  { title: "Client Support", logo: "bar5.svg" },
+  { title: "Streamlined Processes", logo: "bar6.svg" },
+];
---
+++ src/data/list1.js
@@ -0,0 +1,11 @@
+export const list1 = [
+  { title: "Artificial Inteligence (AI) Models" },
+  { title: "Talent Evaluation Models" },
+  { title: "Talent Performance Models" },
+  { title: "Risk Management Models" },
+  { title: "Social Media Models" },
+  { title: "Marketing and Advertising Models" },
+  { title: "Financial Models" },
+  { title: "Biometric Models" },
+  { title: "Innovation Management Models" },
+];
