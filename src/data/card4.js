616892f4e56eda2c04ad393e732846c20513429b
matti909 <matias.saantiago@gmail.com>
2024-07-08 09:56:26 -0300

    fixed

--- src/data/card4.js
+++ src/data/card4.js
@@ -1,9 +1,9 @@
-import Bar1 from "../pages/services/icons/Bar1.astro";
-import Bar2 from "../pages/services/icons/Bar2.astro";
-import Bar3 from "../pages/services/icons/Bar3.astro";
-import Bar4 from "../pages/services/icons/Bar4.astro";
-import Bar5 from "../pages/services/icons/Bar5.astro";
-import Bar6 from "../pages/services/icons/Bar6.astro";
+import Bar1 from "../components/icons/Bar1.astro";
+import Bar2 from "../components/icons/Bar2.astro";
+import Bar3 from "../components/icons/Bar3.astro";
+import Bar4 from "../components/icons/Bar4.astro";
+import Bar5 from "../components/icons/Bar5.astro";
+import Bar6 from "../components/icons/Bar6.astro";
 
 export const card4 = [
   { title: "Unmatched Expertise", logo: Bar1 },
