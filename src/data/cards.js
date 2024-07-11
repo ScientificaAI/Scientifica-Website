7805443052c67ac4a742aaf31c351e376c5f6552
matti909 <matias.saantiago@gmail.com>
2024-07-11 10:14:34 -0300

    count up added

--- src/data/cards.js
+++ src/data/cards.js
@@ -4,8 +4,8 @@ import IconWork from "../components/IconWork.astro";
 import IconWorld from "../components/IconWorld.astro";
 
 export const cards = [
-  { title: "200+", body: "Happy clients", logo: IconPeople },
-  { title: "100M+", body: "Models developed", logo: IconCross },
-  { title: "300+", body: "Products build", logo: IconWork },
-  { title: "15+", body: "Countries served", logo: IconWorld },
+  { title: "200", body: "Happy clients", logo: IconPeople },
+  { title: "100", body: "Models developed", logo: IconCross },
+  { title: "300", body: "Products build", logo: IconWork },
+  { title: "15", body: "Countries served", logo: IconWorld },
 ];
