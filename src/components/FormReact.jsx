99d8c0caa7b79608929773c121746d6cfd1a58fd
matti909 <matias.saantiago@gmail.com>
2024-07-16 16:34:51 -0300

    form v1 ready

--- src/components/FormReact.jsx
+++ src/components/FormReact.jsx
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { useState } from "react";
 import InputForm from "./InputForm";
 import SelectForm from "./SelectForm";
+import { Toaster, toast } from "sonner";
 
 // Inicializa el cliente de Supabase
 const supabase = createClient(
@@ -82,10 +83,12 @@ const Formulario = () => {
       country,
       studies,
     });
+    console.log(result);
 
     if (!result) {
       setMessage("Error al enviar los datos: " + error.message);
     } else {
+      toast("Success");
       setMessage("Datos enviados con éxito!");
       setFormData({
         first_name: "",
@@ -104,120 +107,136 @@ const Formulario = () => {
           },
         ], */
       });
+      window.location.href = "/";
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col">
-      {/* Personal Information */}
-      <div className="grid gap-4 mb-4 sm:grid-cols-2">
-        <InputForm
-          name="first_name"
-          type="text"
-          value={formData.first_name}
-          onChange={handleChange}
-          placeholder=" "
-        />
-        <InputForm
-          name="last_name"
-          type="text"
-          value={formData.last_name}
-          onChange={handleChange}
-          placeholder=" "
-        />
-        <InputForm
-          name="email"
-          type="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder=" "
-        />
-        <SelectForm
-          label="country"
-          name="country"
-          value={formData.country}
-          onChange={handleChange}
-          placeholder={"country"}
-          options={options}
-        />
-      </div>
+    <>
+      <Toaster />
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        {/* Personal Information */}
+        <div className="grid gap-4 mb-4 sm:grid-cols-2">
+          <InputForm
+            name="first_name"
+            type="text"
+            value={formData.first_name}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <InputForm
+            name="last_name"
+            type="text"
+            value={formData.last_name}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <InputForm
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <SelectForm
+            label="country"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            placeholder={"country"}
+            options={options}
+          />
+        </div>
 
-      {/* Studies Information */}
+        {/* Studies Information */}
 
-      <>
-        {formData.studies.map((study, index) => (
-          <div key={index} className="mb-10">
-            <div class="pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600 py-10">
-              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                Studies
-              </h3>
+        <>
+          {formData.studies.map((study, index) => (
+            <div key={index} className="mb-1">
+              <div class="pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600 py-10">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                  Studies
+                </h3>
+              </div>
+              <div className="grid gap-4  sm:grid-cols-2">
+                <InputForm
+                  type="text"
+                  name="degree"
+                  value={formData.degree}
+                  onChange={(e) => handleChange(e, index, "studies")}
+                  id="degree"
+                  placeholder=" "
+                  required
+                />
+                <InputForm
+                  type="text"
+                  name="university"
+                  value={formData.university}
+                  onChange={(e) => handleChange(e, index, "studies")}
+                  id="university"
+                  placeholder=" "
+                  required
+                />
+                <InputForm
+                  type="text"
+                  name="graduation"
+                  value={formData.graduation}
+                  onChange={(e) => handleChange(e, index, "studies")}
+                  id="graduation"
+                  placeholder=" "
+                  required
+                />
+              </div>
             </div>
-            <div className="grid gap-4 mb-4 sm:grid-cols-2">
-              <InputForm
-                type="text"
-                name="degree"
-                value={formData.degree}
-                onChange={(e) => handleChange(e, index, "studies")}
-                id="degree"
-                placeholder=" "
-                required
-              />
-              <InputForm
-                type="text"
-                name="university"
-                value={formData.university}
-                onChange={(e) => handleChange(e, index, "studies")}
-                id="university"
-                placeholder=" "
-                required
-              />
-              <InputForm
-                type="text"
-                name="graduation"
-                value={formData.graduation}
-                onChange={(e) => handleChange(e, index, "studies")}
-                id="graduation"
-                placeholder=" "
-                required
-              />
+          ))}
+
+          <button
+            id="btn-action"
+            onClick={() => handleAddSection("studies")}
+            type="button"
+            class="text-[#0024ff] mb-10 inline-flex items-center bg-transparent font-medium rounded-lg text-sm text-center"
+          >
+            <svg
+              class="w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Add studies
+          </button>
+        </>
+
+        {/* Experience Information */}
+        {/* 
+        {formData.experiences.map((experience, index) => (
+          <div key={index} className="mb-10">
+            <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                Experience
+              </h3>
             </div>
           </div>
         ))}
-
+ */}
+        {message && (
+          <div className="sm:col-span-2 text-center text-green-600">
+            {message}
+          </div>
+        )}
         <button
-          id="btn-action"
-          onClick={() => handleAddSection("studies")}
-          type="button"
-          class="text-blue-500 inline-flex items-center bg-transparent font-medium rounded-lg text-sm text-center"
+          type="submit"
+          className="sm:col-span-2 py-2 px-4 bg-[#0024ff] text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
         >
-          <svg
-            class="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          Add studies
+          Send
         </button>
-      </>
-
-      {message && (
-        <div className="sm:col-span-2 text-center text-green-600">
-          {message}
-        </div>
-      )}
-      <button
-        type="submit"
-        className="sm:col-span-2 py-2 px-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
-      >
-        Send
-      </button>
-    </form>
+      </form>
+    </>
   );
 };
 
