1263113fb21825c774e8cb7f1b1ec124280a61b5
matti909 <matias.saantiago@gmail.com>
2024-07-16 23:10:08 -0300

    simple form added

--- src/components/FormReact.jsx
+++ src/components/FormReact.jsx
@@ -5,7 +5,7 @@ import InputForm from "./InputForm";
 import SelectForm from "./SelectForm";
 
 // Inicializa el cliente de Supabase
-const supabase = createClient(
+export const supabase = createClient(
   "https://cqdcqwnibdsnijtbgafa.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxZGNxd25pYmRzbmlqdGJnYWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0OTgyODMsImV4cCI6MjAzNTA3NDI4M30.s15-GrZmSAUQgXtFCG235m1mtX-9zgUBP1iatghNKQk"
 );
---
+++ src/components/form/SimpleForm.jsx
@@ -0,0 +1,142 @@
+import { supabase } from "../FormReact";
+import { Toaster, toast } from "sonner";
+import { useState } from "react";
+import InputForm from "../InputForm";
+import SelectForm from "../SelectForm";
+
+const TextAreaForm = ({ name, value, onChange, placeholder }) => {
+  return (
+    <div className="mb-4">
+      <textarea
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full px-3 py-2 text-base leading-6 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        rows="4"
+      />
+    </div>
+  );
+};
+
+const SimpleForm = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    country: "",
+    message: "",
+  });
+
+  const options = [
+    { value: "US", label: "United States" },
+    { value: "CA", label: "Canada" },
+    { value: "FR", label: "France" },
+    { value: "DE", label: "Germany" },
+  ];
+
+  const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Procesar los datos del formulario
+    const { first_name, last_name, email, country, message } = formData;
+
+    try {
+      const result = await supabase.from("clients").insert({
+        first_name,
+        last_name,
+        email,
+        country,
+        message,
+      });
+
+      if (!result.error) {
+        toast("Success");
+        setMessage("Datos enviados con éxito!");
+        setFormData({
+          first_name: "",
+          last_name: "",
+          email: "",
+          country: "",
+          message: "",
+        });
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 2000);
+      } else {
+        setMessage("Error al enviar los datos: " + result.error.message);
+      }
+    } catch (error) {
+      setMessage("Error al enviar los datos: " + error.message);
+    }
+  };
+
+  return (
+    <div>
+      <Toaster />
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        {/* Personal Information */}
+        <div className="grid gap-4 mb-4 sm:grid-cols-2">
+          <InputForm
+            name="first_name"
+            type="text"
+            value={formData.first_name}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <InputForm
+            name="last_name"
+            type="text"
+            value={formData.last_name}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <InputForm
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder=" "
+          />
+          <SelectForm
+            label="country"
+            name="country"
+            value={formData.country}
+            onChange={handleChange}
+            placeholder={"country"}
+            options={options}
+          />
+        </div>
+        <TextAreaForm
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          placeholder="Leave us a message"
+        />
+        {message && (
+          <div className="sm:col-span-2 text-center text-green-600">
+            {message}
+          </div>
+        )}
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className={`sm:col-span-2 py-2 px-4 bg-[#0024ff] text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 ${
+            isSubmitting ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+        >
+          {isSubmitting ? "Sending..." : "Send"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
