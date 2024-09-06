a4746d2089e3af5d78079285c4c1fed82acec8b7
matti909 <matias.saantiago@gmail.com>
2024-09-06 12:07:27 -0300

    ready production stage

--- src/components/FormReact.jsx
+++ src/components/FormReact.jsx
@@ -376,7 +376,7 @@ const Formulario = () => {
     e.preventDefault();
     const form = formRef.current;
     const endPoint =
-      "https://3lm8epc41j.execute-api.us-east-1.amazonaws.com/items";
+      "https://soqlkgd3j6.execute-api.us-east-1.amazonaws.com/items";
 
     const id2 = uuidv4();
 
