1f7f2987948697af2e0ab3fb9c0ebd19a9c86433
matti909 <matias.saantiago@gmail.com>
2024-09-11 11:01:41 -0300

    aca funciona

--- serverless/aws-node-express-dynamodb-api-project/index.js
+++ serverless/aws-node-express-dynamodb-api-project/index.js
@@ -25,7 +25,7 @@ app.options("*", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://scientifica.ai", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
