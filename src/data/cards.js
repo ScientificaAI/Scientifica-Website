5759fa369184e23c15304cfea767f889649c3cb4
matti909 <matias.saantiago@gmail.com>
2024-10-07 09:43:10 -0300

    card scientist fixed

--- src/data/cards.js
+++ src/data/cards.js
@@ -4,7 +4,7 @@ import IconWork from "../components/IconWork.astro";
 import IconWorld from "../components/IconWorld.astro";
 
 export const cards = [
-  { title: "200", body: "Happy clients", logo: IconPeople },
+  { title: "200", body: "Scientists", logo: IconPeople },
   { title: "100", body: "Models developed", logo: IconCross },
   { title: "50", body: "Products build", logo: IconWork },
   { title: "35", body: "Countries served", logo: IconWorld },
