594596fd4cbee53fb71f074b06191e8e69220363
matti909 <matias.saantiago@gmail.com>
2024-10-15 12:03:56 -0300

    fixed cards on research page

--- src/data/card5.js
+++ src/data/card5.js
@@ -6,12 +6,12 @@ import Tm4 from "../components/Tm4.astro";
 export const card5 = [
   {
     title: "Grow business with knowledge-rich research",
-    body: "Truly valuable innovation is driven by data and newly acquired knowledge and research is the key to acquiring both. At scientifica.ai, our research services encompass your business R&D and research needs. We design and conduct qualitative studies and quantitative research to find answers to the most challenging questions you have utilizing an array of sophisticated research methodologies. Our R&D service provides you with the knowledge to make critical business decisions and build new products with conviction and sound information.",
+    body: "Truly valuable innovation is driven by data and newly acquired knowledge and research is the key to acquiring both. At scientifica, our research services encompass your business R&D and research needs. We design and conduct qualitative studies and quantitative research to find answers to the most challenging questions you have utilizing an array of sophisticated research methodologies. Our R&D service provides you with the knowledge to make critical business decisions and build new products with conviction and sound information.",
     logo: Tm1,
   },
   {
     title: "Comprehensive research services for reliable business decisions",
-    body: "Scientifica.ai is a business R&D powerhouse that is dedicated to filling the research gap for partner businesses. Our research services are delivered by a team of professional data scientists and researchers who have stellar academic and research credentials. Our R&D services create knowledge assets that help clients build groundbreaking products and impactful business decisions with the potential to change the world.",
+    body: "Scientifica is a business R&D powerhouse that is dedicated to filling the research gap for partner businesses. Our research services are delivered by a team of professional data scientists and researchers who have stellar academic and research credentials. Our R&D services create knowledge assets that help clients build groundbreaking products and impactful business decisions with the potential to change the world.",
     logo: Tm2,
   },
   {
